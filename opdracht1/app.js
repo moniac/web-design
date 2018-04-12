04c94049ad00138a80a09ecc4b1d2a7bf674d313
Mohammed Mulazada <thisismoniac@gmail.com>
2018-04-12 09:40:49 +0200

    added readme

--- opdracht1/app.js
+++ opdracht1/app.js
@@ -1,13 +1,13 @@
 import view from './view.js'
 
-if (localStorage.getItem('label')) {
-	console.log('ye')
-	const label = document.querySelector('svg')
-	label.style.transform = 'translateY(0)';
-}
+// if (localStorage.getItem('label')) {
+// 	console.log('ye')
+// 	const label = document.querySelector('svg')
+// 	label.style.transform = 'translateY(0)';
+// }
 
-function deleteStorage() {
-	localStorage.removeItem('label')
-}
+// function deleteStorage() {
+// 	localStorage.removeItem('label')
+// }
 
 view.getPopular()
