7bb180fe6713861637b7f1f1cc1aa8fd71ea9685
Mohammed Mulazada <thisismoniac@gmail.com>
2018-04-19 01:05:13 +0200

    config

---
+++ opdracht2/postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	plugins: [
+		require('postcss-import'),
+		require('postcss-nesting'),
+		require('postcss-custom-properties'),
+		require('postcss-cssnext')({
+			browsers: ['ie >= 8'],
+			features: {
+				rem: true,
+				customProperties: {
+					strict: false,
+					warnings: true,
+					preserve: true
+				}
+			}
+		}),
+		require('cssnano')({
+			autoprefixer: false
+		})
+	]
+}
