420b83c50f7f0c667a8375ccc0fb96d5fadbbf27
Mohammed Mulazada <thisismoniac@gmail.com>
2018-04-21 00:50:12 +0200

    Added lazyloading

--- opdracht2/src/postcss.config.js
+++ opdracht2/src/postcss.config.js
@@ -2,25 +2,6 @@ module.exports = {
 	plugins: [
 		require('postcss-import'),
 		require('postcss-nesting'),
-		require('postcss-custom-properties'),
-		require('postcss-discard-comments')({
-			discardComments: {
-				removeAll: true
-			}
-		}),
-		require('postcss-cssnext')({
-			browsers: ['ie >= 8'],
-			features: {
-				rem: true,
-				customProperties: {
-					strict: false,
-					warnings: false,
-					preserve: true
-				}
-			}
-		}),
-		require('cssnano')({
-			autoprefixer: false
-		})
+		require('postcss-custom-media')
 	]
 }
